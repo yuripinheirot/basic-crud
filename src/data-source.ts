fdb7fec3c1442316ee1a7d6f35f3668cd41eeff9
Yuri Pinheiro <yuripinheirot@gmail.com>
2022-11-20 16:52:45 -0400

    feat: add typeorm config

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,12 +3,12 @@ import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
+    type: "mongodb",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT as unknown as number,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
     entities: [User],

01d1fb9c09a74e29b0a60dd9c77a6633622e95b4
Yuri Pinheiro <yuripinheirot@gmail.com>
2022-11-21 13:15:26 -0400

    feat: finished configuration mongo

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,17 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import { User } from './entity/User'
 
 export const AppDataSource = new DataSource({
-    type: "mongodb",
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT as unknown as number,
-    // username: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
+  type: 'mongodb',
+  host: 'db',
+  port: 27017,
+  // username: process.env.DB_USERNAME,
+  // password: process.env.DB_PASSWORD,
+  database: 'basic-crud',
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
 })
